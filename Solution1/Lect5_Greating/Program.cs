609b2b79bbf58a67c718264d6c978d7e565d0904
Андрей Васильев <puhlikvinni@gmail.com>
2020-03-19 13:41:00 +0300

    Lect5_Greating

---
+++ Solution1/Lect5_Greating/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lect5_Greating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan time = DateTime.Now.TimeOfDay;
+            if(time.Hours > 8 && time.Hours < 12)
+            {
+                Console.WriteLine(" Good Morning, guys");
+            }
+            else if (time.Hours > 11 && time.Hours < 15)
+            {
+                Console.WriteLine(" Good Day, guys");
+            }
+            else if (time.Hours > 14 && time.Hours < 22)
+            {
+                Console.WriteLine(" Good Evening, guys");
+            }
+            else
+            {
+                Console.WriteLine("Bad Idea");
+            }
+        }
+    }
+}
