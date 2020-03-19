787f7e698baae3693f8c4d5df69512d43cd6a3c0
Андрей Васильев <puhlikvinni@gmail.com>
2020-03-19 13:31:02 +0300

    Lect5_LetterA

---
+++ Solution1/Lect5_LetterA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lect5_LetterA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(text[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            
+            Console.WriteLine(count);
+        }
+    }
+}
