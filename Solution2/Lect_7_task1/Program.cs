3863910a864ef1206de870c2598cf91817f19572
Андрей Васильев <puhlikvinni@gmail.com>
2020-03-24 19:55:50 +0300

    task1

---
+++ Solution2/Lect_7_task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lect_7_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void Task1()
+        {
+            int[,,] array1 = new int[2,2,3];
+        }
+    }
+}
