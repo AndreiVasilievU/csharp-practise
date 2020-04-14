2f01228fc335d51905b599f6d7cf0fe90476eced
Андрей Васильев <puhlikvinni@gmail.com>
2020-04-14 21:54:25 +0300

    lect_11_practise_shape

---
+++ Solution3/Lect_11_Shapes/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lect_11_Shapes
+{
+    class Circle : Shape
+    {
+        int radius = 5;
+        public override void SquareShape()
+        {
+            int SqS = radius * radius;
+        }
+        public override void Name()
+        {
+            Console.WriteLine("Circle");
+        }
+    }
+}
---
+++ Solution3/Lect_11_Shapes/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lect_11_Shapes
+{
+    class Shape
+    {
+        public virtual void SquareShape()
+        {
+
+        }
+        public virtual void Name()
+        {
+
+        }
+    }
+}
