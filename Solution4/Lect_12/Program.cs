3620bc0941263ddbd05ea3e3665e34d985c8c2ad
Андрей Васильев <puhlikvinni@gmail.com>
2020-04-16 20:35:18 +0300

    ((

---
+++ Solution4/Lect_12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lect_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public static class UniqueItem
+    {
+        static int id = 0;
+    }
+}
